c1d6dd56504668d2f7e2d32d4117439cbef30f48
kasa <dxxx.asian.pride.0331@gmail.com>
2023-06-25 11:54:14 +0000

    2-11

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,18 +1,24 @@
 class TopicsController < ApplicationController
-  def new
-    render :new
-  end
-  # ここから
+   def index
+    @title = params[:title]
+    if @title.present?
+      @posts = Post.where('title LIKE ?', "%#{@title}%")
+    else
+      @posts = Post.all
+    end
+    render :index
+   end
+
   def new
     @post = Post.new
     render :new
   end
-
+  
   def create
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to new_topic_path, notice: '登録しました'
+      redirect_to index_topic_path, notice: '登録しました'
     else
       render :new, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topic'
   post 'topics/new', to: 'topics#create', as: 'create_topic'
   post 'topics/edit', to: 'topics#update', as: 'update_topics'
+  get 'topics/index', to: 'topics#index', as: 'index_topic'
 end
